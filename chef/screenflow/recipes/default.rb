b86c901993a5245490c9f6c9cd825eccbedba077
Chris Patuzzo <chris@patuzzo.co.uk>
2019-03-23 11:31:23 +0000

    Don’t install screenflow
    I’m now installing this from the App store since I
    bought it.

--- chef/screenflow/recipes/default.rb
+++
@@ -1,9 +0,0 @@
-ruby_block "install screenflow" do
-  not_if { ZZ::Exec.cask_installed?("screenflow") }
-  block { ZZ::Exec.install_cask("screenflow") }
-end
-
-ruby_block "configure screenflow" do
-  not_if { ZZ::Exec.screenflow_configured? }
-  block { ZZ::Exec.configure_screenflow }
-end
