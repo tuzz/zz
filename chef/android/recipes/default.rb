5c7a564a680d305a9231bbe44305c94b4384a7c5
Chris Patuzzo <chris@patuzzo.co.uk>
2020-07-07 14:00:46 +0100

    Remove Android Studio

--- chef/android/recipes/default.rb
+++
@@ -1,6 +0,0 @@
-ruby_block "install android studio" do
-  not_if { ZZ::Exec.cask_installed?("android-studio") }
-  block { ZZ::Exec.install_cask("android-studio") }
-end
-
-# TODO: set up a virtual device, e.g. Google Pixel
